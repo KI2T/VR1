93bb39ac9dd9afdde4f0786d6d579b2e7e1f10ad
KITT <Sillyuser@yandex.ru>
2018-09-27 10:55:19 +0300

    0

---
+++ PandaHackVR2/Introduction-To-HTC-Vive-Final для Unity5.5/Introduction To HTC Vive - 2/Assets/CollTb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollTb : MonoBehaviour {
+
+    public Rigidbody RwCube;
+    public Transform cell;
+
+
+    void OnTriggerEnter(Collider ControllerRight)
+    {
+        Instantiate(RwCube, cell.position, cell.rotation);  
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PandaHackVR2/Introduction-To-HTC-Vive-Final для Unity5.5/Introduction To HTC Vive - 2/Assets/cr_h.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cr_h : MonoBehaviour {
+
+    public Rigidbody newelem;
+    public Transform NEW_ELEM;
+
+
+    void OnTriggerEnter()
+    {
+        Instantiate(newelem, NEW_ELEM.position, NEW_ELEM.rotation);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
