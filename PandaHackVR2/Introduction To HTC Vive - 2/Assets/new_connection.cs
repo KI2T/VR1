f8ace126779d2442ccf24b191ba27cdc94bb32a5
KITT <Sillyuser@yandex.ru>
2018-09-27 13:03:58 +0300

    0

---
+++ PandaHackVR2/Introduction To HTC Vive - 2/Assets/new_connection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new_connection : MonoBehaviour {
+
+        public Rigidbody newelem;
+        public Transform lastpos;
+
+
+        void OnTriggerEnter()
+        {
+            Instantiate(newelem, lastpos.position, newelem.rotation);
+
+        }
+
+
+
+        // Use this for initialization
+        void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
